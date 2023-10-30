a81fed14290979a943e24403aaafbe27613b7da7
Emad <empsychoxlive@yahoo.com>
2023-10-30 10:16:39 -0700

    update javascript file pt-1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-e:\Coding\HomePage(friendConnect)\miniProject-Using-HTML-CSS-JS\assets
+'use strict'
+const inputEl=document.querySelector('#InputExplore')
+const closeEl=document.querySelector('.close')
+const userSearchBar=document.querySelector('.userSearchBar')
+inputEl.addEventListener('click', ()=>{
+    userSearchBar.classList.remove('nonActive')
+    closeEl.classList.remove('nonActive')
+    document.body.style.overflow='hidden'
+})
+closeEl.addEventListener('click', ()=>{
+    userSearchBar.classList.add('nonActive')
+    closeEl.classList.add('nonActive')
+    document.body.style.overflow='auto'
+})
