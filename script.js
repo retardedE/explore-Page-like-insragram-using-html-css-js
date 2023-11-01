6d28f0c64951210ab7725b35978d85dcd33a9f9a
Emad <empsychoxlive@yahoo.com>
2023-11-01 04:43:35 -0700

    update javascript file last pt

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ closeEl.addEventListener('click', ()=>{
     closeEl.classList.add('nonActive')
     document.body.style.overflow='auto'
 })
+const postBox=document.querySelectorAll('.postBox')
+const clickedPost=document.querySelectorAll('.clickedPost')
+const closePost=document.querySelectorAll('.closePost')
+let isOpen=false
+let arrayBoxes= Array.from(postBox)
+let arrayBoxesClicked= Array.from(clickedPost)
+let arrayClosePost= Array.from(closePost)
+for(let i=0; i<arrayBoxes.length;i++){
+    arrayBoxes[i].addEventListener('click', ()=>{
+        arrayBoxesClicked[i].classList.remove('nonActive')
+    })
+}
